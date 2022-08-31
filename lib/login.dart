0a865aac00f101630b5ae431c15cd82092b6b96d
Rawin Acharya <acharyarabin8@gmail.com>
2022-08-31 20:57:50 +0545

    log in

--- lib/login.dart
+++ lib/login.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: deprecated_member_use, unnecessary_new, avoid_unnecessary_containers, non_constant_identifier_names, library_private_types_in_public_api, duplicate_ignore, avoid_print
 
 import 'package:flutter/material.dart';
-import 'package:wc_form_validators/wc_form_validators.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -17,13 +16,12 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     // ignore: prefer_typing_uninitialized_variables
-    var Validators;
     return Scaffold(
       body: Center(
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Image.asset('assets/images/iamges.png', scale: 4.0),
+              Image.asset('assets/images/images.png', scale: 5.0),
               Container(
                 padding: const EdgeInsets.all(10),
                 child: Form(
@@ -48,10 +46,6 @@ class _LoginState extends State<Login> {
                                       const BorderSide(
                                           width: 1, color: Colors.grey)),
                               labelText: 'Email'),
-                          validator: Validators.compose([
-                            Validators.required('email is required'),
-                            Validators.email('invalid email address')
-                          ]),
                         ),
                       ),
                       const SizedBox(
@@ -70,8 +64,6 @@ class _LoginState extends State<Login> {
                                       const BorderSide(
                                           width: 1, color: Colors.grey)),
                               labelText: 'Password'),
-                          validator: Validators.compose(
-                              [Validators.required('password is required')]),
                         ),
                       ),
                       const SizedBox(
